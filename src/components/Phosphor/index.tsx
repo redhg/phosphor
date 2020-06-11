3ef675b0b19bc190fc7908c5ae5ce24b3c8551d2
Breon Halling <github@redhg.com>
2020-06-10 22:40:24 -0400

    Remove reference to ypsilon json

--- src/components/Phosphor/index.tsx
+++ src/components/Phosphor/index.tsx
@@ -16,8 +16,8 @@ import Modal from "../Modal";
 import Scanlines from "../Scanlines";
 
 // import sample data for development purposes
-// import json from "../../data/sample.json";
-import json from "../../data/ypsilon14.json";
+import json from "../../data/sample.json";
+// import json from "../../data/ypsilon14.json";
 
 interface AppState {
     screens: Screen[];
