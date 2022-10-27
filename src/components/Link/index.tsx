6f10db4ca5084fa2c3ab711074207f26e59dd186
Breon Halling <github@redhg.com>
2022-10-26 22:32:15 -0400

    Add support for two finger tap

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -24,52 +24,47 @@ export interface LinkProps {
     onRendered?: () => void;
 }
 
-const LONGPRESS_DURATION = 500; // 0.5 seconds = one long press
+const LONGPRESS_DURATION = 1000; // 0.5 seconds = one long press
 
 const Link: FC<LinkProps> = (props) => {
     const { text, target, className, onClick, onRendered, locked } = props;
     const css = ["__link__", className ? className : null].join(" ").trim();
 
-    const [timer, setTimer] = useState<number>(null);
-    const [isLongPress, setIsLongPress] = useState(false);
+    // const [timer, setTimer] = useState<number>(null);
+    // const [isLongPress, setIsLongPress] = useState(false);
 
-    // const [startTime, setStartTime] = useState<number>(null);
+    // const [startTime, setStartTime] = useState<number>(0);
 
     // events
-    let ms = 0;
-    const handleMouseDown = (e: React.MouseEvent<HTMLSpanElement>) => {
-        // start the timer
-        ms = 0;
-        setIsLongPress(false);
-        window.clearInterval(timer);
-        setTimer(null);
-        setTimer(window.setInterval(() => {
-            ms += 10;
-            if (ms === LONGPRESS_DURATION) {
-                setIsLongPress(true);
-            }
-        }, 10));
-    };
+    // let ms = 0;
+    // const handleMouseDown = (e: React.MouseEvent<HTMLSpanElement>) => {
+    //     setStartTime(Date.now());
+    // };
 
     const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
-        const unlocked = e.shiftKey || isLongPress;
-        onClick && onClick(target, unlocked);
-        // clear the timer if necessary
-        window.clearInterval(timer);
-        setTimer(null);
+        e.preventDefault();
+        console.log("click");
+        onClick && onClick(target, e.shiftKey);
     };
-    const handleMouseUp = (e: React.MouseEvent<HTMLSpanElement>) => {
-        // clear the timer
-        window.clearInterval(timer);
-        setTimer(null);
-    }
     const handleRendered = () => (onRendered && onRendered());
 
     // this should fire on mount/update
     useEffect(() => handleRendered());
 
+    const handleTouchEnd = (e: React.TouchEvent<HTMLSpanElement>) => {
+        e.preventDefault(); // prevents the click event firing
+        console.log("handleTouchEnd");
+        onClick && onClick(target, e.touches.length > 1);
+    };
+
     return (
-        <span className={css} onMouseDown={handleMouseDown} onClick={handleClick} onMouseUp={handleMouseUp}>{text}</span>
+        <span
+            className={css}
+            onClick={handleClick}
+            onTouchEnd={handleTouchEnd}
+        >
+            {text}
+        </span>
     );
 };
 
